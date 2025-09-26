2b1589054752496b5b7b5387980f4947b918387d
186480 <186480@stud.hvl.no>
2025-09-26 15:26:10 +0200

    op

---
+++ Oblig3/src/test/java/com/oblig3_DAT108/Oblig/Oblig3Dat108ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oblig3_DAT108.Oblig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Dat108ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
